a5b83c14f2d0bd0ee4829b4abe1a8df2a4eb39e6
Mr. Ydav <y.dav360@gmail.com>
2026-08-21 20:26:57 +0300

    Add ZoomBox

---
+++ src/ZeppelinForms/Forms/Controls/Base/ZoomBox.cs
@@ -0,0 +1,24 @@
+﻿using ZeppelinForms.Drawing;
+using ZeppelinForms.Forms.Interfaces;
+
+namespace ZeppelinForms.Forms.Controls.Base;
+
+/// <summary>
+/// Allow zoom in\out for child
+/// </summary>
+public class ZoomBox : UIElement, IInputElement
+{
+    public bool IsFocused { get; set; }
+    public bool TabStop { get; set; }
+    public uint TabIndex { get; set; }
+
+    public override void Draw(Graphics g)
+    {
+        
+    }
+
+    public void Zoom(float factor)
+    {
+        // TODO: zoom child
+    }
+}
