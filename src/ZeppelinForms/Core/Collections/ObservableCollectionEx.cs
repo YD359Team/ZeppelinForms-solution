6c65a609778888ca7b3d775aa839d5d6f36a1e03
Mr. Ydav <y.dav360@gmail.com>
2026-08-22 00:30:35 +0300

    Add ObservableCollectionEx

---
+++ src/ZeppelinForms/Core/Collections/ObservableCollectionEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ZeppelinForms.Core.Collections;
+
+internal static class ObservableCollectionEx
+{
+    extension<T> (ObservableCollection<T> oc)
+    {
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                oc.Add(item);
+            }
+        }
+    }
+}
