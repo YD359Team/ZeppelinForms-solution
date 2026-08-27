a4a854fd6d6a6ec88bc0c202ec1e042d4db26039
Mr. Ydav <y.dav360@gmail.com>
2026-08-28 02:00:45 +0300

    Add GridLengthTest

---
+++ tests/ZeppelinForms.UnitTests/ControlTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ZeppelinForms.Forms.Controls;
+
+namespace ZeppelinForms.UnitTests;
+
+public class ControlTests
+{
+    [Fact]
+    public void GridLengthTest()
+    {
+        Grid grid = new Grid
+        {
+            Columns = "150,0.2*,0.75*"
+        };
+        Assert.True(!grid.ColumnDefinitions[0].IsStar);
+        Assert.True(grid.ColumnDefinitions[1].IsStar);
+        Assert.True(grid.ColumnDefinitions[2].IsStar);
+    }
+}
