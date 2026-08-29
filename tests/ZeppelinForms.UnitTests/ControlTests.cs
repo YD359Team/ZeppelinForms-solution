36c415393adaf7bf7cd88d7ea4efae22be37e8ef
Mr. Ydav <y.dav360@gmail.com>
2026-08-30 00:34:52 +0300

    Add new tests

--- tests/ZeppelinForms.UnitTests/ControlTests.cs
+++ tests/ZeppelinForms.UnitTests/ControlTests.cs
@@ -11,12 +11,35 @@ public class ControlTests
     [Fact]
     public void GridLengthTest()
     {
-        Grid grid = new Grid
+        Grid grid = new()
         {
             Columns = "150,0.2*,0.75*"
         };
         Assert.True(!grid.ColumnDefinitions[0].IsStar);
         Assert.True(grid.ColumnDefinitions[1].IsStar);
         Assert.True(grid.ColumnDefinitions[2].IsStar);
+    }
+
+    [Fact]
+    public void ListBoxItemsTest()
+    {
+        ListBox listBox = new();
+        listBox.Items.Add(1);
+        listBox.Items.Add(25);
+        listBox.Items.Add(-4);
+
+        Assert.True(listBox.Items.Count == 3);
+    }
+
+    [Fact]
+    public void ComboBoxItemsTest()
+    {
+        ComboBox comboBox = new();
+        comboBox.Items.Add(3);
+        comboBox.Items.Add(-66);
+        comboBox.Items.Add(1234);
+        comboBox.Items.Add(555);
+
+        Assert.True(comboBox.Items.Count == 4);
     }
 }
