32573f05b6ae681b53c14f6366063c09ced895b8
Mr. Ydav <y.dav360@gmail.com>
2026-08-30 19:51:22 +0300

    Fix TabItem scope

--- src/ZeppelinForms/Forms/Controls/TabItem.cs
+++ src/ZeppelinForms/Forms/Controls/TabItem.cs
@@ -2,13 +2,10 @@
 
 namespace ZeppelinForms.Forms.Controls;
 
-public readonly partial record struct GridLength
+public sealed class TabItem
 {
-    public sealed class TabItem
-    {
-        public string Header { get; set; } = string.Empty;
-        public string? PathData { get; set; }
-        public UIElement? Content { get; set; }
-        public bool IsEnabled { get; set; } = true;
-    }
+    public string Header { get; set; } = string.Empty;
+    public string? PathData { get; set; }
+    public UIElement? Content { get; set; }
+    public bool IsEnabled { get; set; } = true;
 }
