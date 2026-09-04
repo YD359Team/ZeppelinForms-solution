82c57d996901ea334307c57f1cf8729eb5cf653d
Mr. Ydav <y.dav360@gmail.com>
2026-09-05 01:38:25 +0300

    Add GripBox base

---
+++ src/ZeppelinForms/Forms/Controls/GripBox.cs
@@ -0,0 +1,11 @@
+﻿using ZeppelinForms.Forms.Controls.Base;
+
+namespace ZeppelinForms.Forms.Controls;
+
+/// <summary>
+/// Control for manual resize and rotate
+/// </summary>
+public class GripBox : DecoratedWrapControl
+{
+    // TODO: 
+}
