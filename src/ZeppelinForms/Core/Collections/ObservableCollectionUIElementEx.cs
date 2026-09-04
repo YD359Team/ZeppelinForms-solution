e53bba15b69a6b364245d0961426851e12f4fb3d
Mr. Ydav <y.dav360@gmail.com>
2026-09-04 13:15:20 +0300

    Add extension for uielement observable collection Add more colors Add MediaColors class

---
+++ src/ZeppelinForms/Core/Collections/ObservableCollectionUIElementEx.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using ZeppelinForms.Forms.Controls.Base;
+
+namespace ZeppelinForms.Core.Collections;
+
+public static class ObservableCollectionUIElementEx
+{
+    extension(ObservableCollection<UIElement> oc)
+    {
+        public void Add(UIElement item, int row, int column)
+        {
+            item.Row = row;
+            item.Column = column;
+            oc.Add(item);
+        }
+    }
+}
--- src/ZeppelinForms/Drawing/Primitives/Colors.cs
+++ src/ZeppelinForms/Drawing/Primitives/Colors.cs
@@ -8,12 +8,13 @@ public static class Colors
     public static readonly Color White = new(255, 255, 255);
 
     public static readonly Color Red = new(255, 0, 0);
-    public static readonly Color Green = new(0, 255, 0);
+    public static readonly Color DarkestGreen = new(0, 255, 0);
     public static readonly Color Blue = new(0, 0, 255);
 
     public static readonly Color Yellow = new(255, 255, 0);
     public static readonly Color Cyan = new(0, 255, 255);
     public static readonly Color Magenta = new(255, 0, 255);
+    public static readonly Color Gold = new(255, 215, 0);
 
     public static readonly Color Gray = new(128, 128, 128);
     public static readonly Color DarkGray = new(64, 64, 64);
@@ -25,11 +26,35 @@ public static class Colors
     public static readonly Color Brown = new(165, 42, 42);
 
     public static readonly Color Lime = new(0, 255, 0);
+    public static readonly Color LimeGreen = new(50, 205, 50);
+    public static readonly Color ForestGreen = new(34, 139, 34);
     public static readonly Color Navy = new(0, 0, 128);
+    public static readonly Color Green = new(0, 128, 0);
     public static readonly Color Teal = new(0, 128, 128);
     public static readonly Color Olive = new(128, 128, 0);
     public static readonly Color Maroon = new(128, 0, 0);
     public static readonly Color Aqua = new(0, 255, 255);
     public static readonly Color Fuchsia = new(255, 0, 255);
     public static readonly Color Silver = new(192, 192, 192);
+    public static readonly Color SkyBlue = new(135, 206, 235);
+    public static readonly Color DeepSkyBlue = new(0, 191, 255);
+
+    public static readonly Color SpringGreen = new(0, 255, 127);
+    public static readonly Color DodgerBlue = new(0x1E, 0x90, 0xFF);
+    public static readonly Color GrannySmithApple = new(0, 155, 118);
+    public static readonly Color Eggplant = new(153, 0, 102);
+    public static readonly Color PrussianBlue = new (0, 49, 83);
+    public static readonly Color PaleGreen = new (152, 251, 152);
+    public static readonly Color BrilliantBlueGreen = new (0, 155, 118);
+    public static readonly Color BrilliantBlue = new (66, 133, 180);
+    public static readonly Color BrilliantPurple = new (117, 93, 154);
+}
+
+public static class MediaColors
+{
+    public static readonly Color Dotnet = new(0x70, 0x37, 0x82);
+    public static readonly Color DotnetModern = new(0x5B, 0x37, 0xD7);
+    public static readonly Color Habrahabr = new(120, 162, 183);
+    public static readonly Color Facebook = new(59, 89, 152);
+    public static readonly Color Twitter = new(31, 174, 233);
 }
